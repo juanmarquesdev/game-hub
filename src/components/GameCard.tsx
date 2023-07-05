c8e01bed2442a870cf856d5f2d408627c1c8a1fb
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-07-05 14:06:23 -0300

    show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import GameCardContainer from "./GameCardContainer";
 
 interface Props {
   game: Game;
@@ -10,17 +11,19 @@ interface Props {
 
 export default function GameCard({ game }: Props) {
   return (
-    <Card width={"300px"} borderRadius={10} overflow={"hidden"}>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <Heading fontSize={"2xl"}>{game.name}</Heading>
-        <HStack justifyContent={"space-between"}>
-          <PlatformIconList
-            platforms={game.platforms.map((p) => p.platform)}
-          ></PlatformIconList>
-          <CriticScore score={game.metacritic}></CriticScore>
-        </HStack>
-      </CardBody>
-    </Card>
+    <GameCardContainer>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <Heading fontSize={"2xl"}>{game.name}</Heading>
+          <HStack justifyContent={"space-between"}>
+            <PlatformIconList
+              platforms={game.platforms.map((p) => p.platform)}
+            ></PlatformIconList>
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </CardBody>
+      </Card>
+    </GameCardContainer>
   );
 }
