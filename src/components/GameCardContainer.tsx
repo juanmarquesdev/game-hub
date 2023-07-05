f3572adafc1d0080cfa1abf167233b1bf5e2d357
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-07-05 14:14:54 -0300

    fetch the genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function GameCardContainer({ children }: Props) {
   return (
-    <Box borderRadius={10} overflow={"hidden"}>
+    <Box width={"300px"} borderRadius={10} overflow={"hidden"}>
       {children}
     </Box>
   );
