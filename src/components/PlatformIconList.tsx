aeee235f59e915e2a217f6273b34a0f413bc3cd7
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-07-05 13:20:03 -0300

    displaying critic game score

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,5 +1,5 @@
 import { Platform } from "../hooks/useGames";
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { HStack, Icon } from "@chakra-ui/react";
 import {
   FaWindows,
   FaPlaystation,
@@ -11,7 +11,6 @@ import {
 import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
-import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
