2a7628140b4bfa8d0da73a47912bb820b2f50add
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-07-05 13:11:05 -0300

    displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,56 @@
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const getIconBySlug = (slug: string) => {
+  if (slug.includes("xbox")) {
+    return FaXbox;
+  } else if (slug.includes("playstation")) {
+    return FaPlaystation;
+  } else if (slug.includes("pc")) {
+    return FaWindows;
+  } else if (slug.includes("nintendo")) {
+    return SiNintendo;
+  } else if (slug.includes("mac")) {
+    return FaApple;
+  } else if (slug.includes("android")) {
+    return FaAndroid;
+  } else if (slug.includes("linux")) {
+    return FaLinux;
+  } else if (slug.includes("ios")) {
+    return MdPhoneIphone;
+  } else if (slug.includes("web")) {
+    return BsGlobe;
+  } else {
+    return null;
+  }
+};
+
+export default function PlatformIconList({ platforms }: Props) {
+  const plataformList = platforms
+    .map((platform) => getIconBySlug(platform.slug))
+    .filter((value, index, array) => {
+      return array.indexOf(value) === index;
+    });
+
+  return (
+    <HStack marginY={"10px"}>
+      {plataformList.map((p) => p && <Icon color={"gray.500"} as={p} />)}
+    </HStack>
+  );
+}
