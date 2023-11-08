012e1fbce09544218f7f66b145b477f83540f175
juanmarquesdev <juanbatistamarques+github@gmail.com>
2023-11-08 11:39:56 -0300

    add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
 import GameCardContainer from "./GameCardContainer";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -21,7 +22,10 @@ export default function GameCard({ game }: Props) {
             ></PlatformIconList>
             <CriticScore score={game.metacritic} />
           </HStack>
-          <Heading fontSize={"2xl"}>{game.name}</Heading>
+          <Heading fontSize={"2xl"}>
+            {game.name}
+            <Emoji rating={game.rating_top} />
+          </Heading>
         </CardBody>
       </Card>
     </GameCardContainer>
